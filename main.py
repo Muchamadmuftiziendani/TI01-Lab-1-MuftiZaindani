20c640d11333fd73dbf352e581146743511b70d8
Muchamad Mufti Ziendani <muftizaindani13@gmail.com>
2020-10-14 03:43:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# SOAL 1 - Menghitung rata-rata
+# Tuliskan program untuk Soal 1 di bawah ini
+
+
+
+
+
+# SOAL 2 - Menulis kelipatan bilangan
+# Tuliskan program untuk Soal 2 di bawah ini
+nama = input("muchamad mufti ziendani")
+print("hallo,", nama, ". belajar pemrograman itu sangat menyenangkan loh dan menarik untuk di pelajari")
+print("sebagai mahasiswa kita harus aktif dalam segi mana pun,ingat mahasiswa itu adalah pembawa perubahan untuk negara")
+print("saya tinggal di bogor jawa barat")
